7014f8d94161b9330a5217e5c0741ca4a6801836
Samuel Lissner <samuel@billymob.com>
2017-02-05 22:03:14 +0100

    Adds planet collision correction

--- src/horoscope/drawer.js
+++ src/horoscope/drawer.js
@@ -11,11 +11,18 @@ export class Drawer {
       throw new Error('Irregular selector');
     }
 
+    this.PLANET_IMAGE_WIDTH = 4;
+    this.PLANET_IMAGE_HEIGHT = 4;
+    this.PLANET_RADIUS_OFFSET = 3;
+    this.PLANET_COLLISION_MARGIN_IN_DEGREE = 4;
+    this.PLANET_COLLISION_CORRECTION_RADIUS = 4;
+    this.MAX_PLANET_COLLISION_CORRECTION = 4;
+
     this.planets = (properties.hasOwnProperty('planets')) ? properties.planets : null;
     this.houses = (properties.hasOwnProperty('houses')) ? properties.houses : null;
 
-    this.s = Snap(this.selector);
-    this.s.attr({viewBox: "-50 -50 100 100"});
+    this.snap = Snap(this.selector);
+    this.snap.attr({viewBox: "-50 -50 100 100"});
 
     this.drawn = {
       circles: this.drawZodiacCircles(),
@@ -45,14 +52,12 @@ export class Drawer {
       ]
     };
 
-    // drawn.planets = this.correctCollidingPlanets(drawn.planets);
+    this.drawn.planets = this.correctCollidingPlanets(this.drawn.planets);
 
     return this.drawn;
   }
 
   describeArc(radius, startDegree, endDegree) {
-
-    const start = Calc.getPointOnCircle(radius, endDegree);
     const end = Calc.getPointOnCircle(radius, startDegree);
     const largeArcFlag = endDegree - startDegree <= 180 ? "0" : "1";
 
@@ -65,9 +70,9 @@ export class Drawer {
 
   drawZodiacCircles() {
     const circles = {
-      outer: this.s.circle(0, 0, zodiac.radius.outer),
-      inner: this.s.circle(0, 0, zodiac.radius.inner),
-      innerAuxiliary: this.s.circle(0, 0, zodiac.radius.innerAuxiliary)
+      outer: this.snap.circle(0, 0, zodiac.radius.outer),
+      inner: this.snap.circle(0, 0, zodiac.radius.inner),
+      innerAuxiliary: this.snap.circle(0, 0, zodiac.radius.innerAuxiliary)
     }
     
     circles.outer.addClass("zodiac-circle-outer");
@@ -87,7 +92,7 @@ export class Drawer {
       const point1 = Calc.getPointOnCircle(radius, degree);
       const point2 = Calc.getPointOnCircle(radius, degree, offsetFromRadius);
 
-      const zodiacDegree = this.s.line(point1.x, point1.y, point2.x, point2.y);
+      const zodiacDegree = this.snap.line(point1.x, point1.y, point2.x, point2.y);
       zodiacDegree.attr({
         index: degree
       });
@@ -138,7 +143,7 @@ export class Drawer {
       const bottomLeftPoint = Calc.getPointOnCircle(zodiac.radius.outer, degreeNextSign);
       const leftArcDescription = this.describeArc(zodiac.radius.outer, degreeNextSign, degree);
 
-      const zodiacSignBackground = this.s.path([
+      const zodiacSignBackground = this.snap.path([
         "M", topLeftPoint.x, topLeftPoint.y,
         "L", topRightPoint.x, topRightPoint.y,
         rightArcDescription,
@@ -158,7 +163,7 @@ export class Drawer {
       const zodiacSignPosition = Calc.getPointOnCircle(zodiac.radius.betweenOuterInner, degreeBetweenSigns)
       const zodiacSignImagePositionX = zodiacSignPosition.x - zodiacSignImageWidth / 2;
       const zodiacSignImagePositionY = zodiacSignPosition.y - zodiacSignImageHeight / 2;
-      const zodiacSignSymbol = this.s.image(signObj.imageUrl, zodiacSignImagePositionX, zodiacSignImagePositionY, zodiacSignImageWidth, zodiacSignImageHeight);
+      const zodiacSignSymbol = this.snap.image(signObj.imageUrl, zodiacSignImagePositionX, zodiacSignImagePositionY, zodiacSignImageWidth, zodiacSignImageHeight);
 
       const meta = {};
       Object.assign(meta, signObj);
@@ -206,7 +211,7 @@ export class Drawer {
     const ascendantDegree = (this.houses.hasOwnProperty('house1') && this.houses.house1.hasOwnProperty('degree')) ? this.houses.house1.degree : null;
     const ascendantPoint = Calc.getPointOnCircle(zodiac.radius.outer, ascendantDegree, -2);
     const descendantPoint = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(ascendantDegree), -2);
-    const ascendantDescendantAxis = this.s.line(ascendantPoint.x, ascendantPoint.y, descendantPoint.x, descendantPoint.y);
+    const ascendantDescendantAxis = this.snap.line(ascendantPoint.x, ascendantPoint.y, descendantPoint.x, descendantPoint.y);
     ascendantDescendantAxis.addClass("house-axis");
     ascendantDescendantAxis.addClass("house-axis-ascendant-descendant");
     return ascendantDescendantAxis;
@@ -216,7 +221,7 @@ export class Drawer {
     const house2Degree = (this.houses.hasOwnProperty('house2') && this.houses.house2.hasOwnProperty('degree')) ? this.houses.house2.degree : null;
     const house2Point = Calc.getPointOnCircle(zodiac.radius.outer, house2Degree, -2);
     const house8Point = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(house2Degree), -2);
-    const house2house8Axis = this.s.line(house2Point.x, house2Point.y, house8Point.x, house8Point.y);
+    const house2house8Axis = this.snap.line(house2Point.x, house2Point.y, house8Point.x, house8Point.y);
     house2house8Axis.addClass("house-axis");
     house2house8Axis.addClass("house-axis-2-8");
     return house2house8Axis;
@@ -226,7 +231,7 @@ export class Drawer {
     const house3Degree = (this.houses.hasOwnProperty('house3') && this.houses.house3.hasOwnProperty('degree')) ? this.houses.house3.degree : null;
     const house3Point = Calc.getPointOnCircle(zodiac.radius.outer, house3Degree, -2);
     const house9Point = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(house3Degree), -2);
-    const house3house9Axis = this.s.line(house3Point.x, house3Point.y, house9Point.x, house9Point.y);
+    const house3house9Axis = this.snap.line(house3Point.x, house3Point.y, house9Point.x, house9Point.y);
     house3house9Axis.addClass("house-axis");
     house3house9Axis.addClass("house-axis-3-9");
     return house3house9Axis;
@@ -236,7 +241,7 @@ export class Drawer {
     const immumCoelliDegree = (this.houses.hasOwnProperty('house4') && this.houses.house4.hasOwnProperty('degree')) ? this.houses.house4.degree : null;
     const immumCoelliPoint = Calc.getPointOnCircle(zodiac.radius.outer, immumCoelliDegree, -2);
     const mediumCoelliPoint = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(immumCoelliDegree), -2);
-    const immumMediumCoelliAxis = this.s.line(immumCoelliPoint.x, immumCoelliPoint.y, mediumCoelliPoint.x, mediumCoelliPoint.y);
+    const immumMediumCoelliAxis = this.snap.line(immumCoelliPoint.x, immumCoelliPoint.y, mediumCoelliPoint.x, mediumCoelliPoint.y);
     immumMediumCoelliAxis.addClass("house-axis");
     immumMediumCoelliAxis.addClass("house-axis-immum-medium");
     return immumMediumCoelliAxis;
@@ -246,7 +251,7 @@ export class Drawer {
     const house5Degree = (this.houses.hasOwnProperty('house5') && this.houses.house5.hasOwnProperty('degree')) ? this.houses.house5.degree : null;
     const house5Point = Calc.getPointOnCircle(zodiac.radius.outer, house5Degree, -2);
     const house11Point = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(house5Degree), -2);
-    const house5house11Axis = this.s.line(house5Point.x, house5Point.y, house11Point.x, house11Point.y);
+    const house5house11Axis = this.snap.line(house5Point.x, house5Point.y, house11Point.x, house11Point.y);
     house5house11Axis.addClass("house-axis");
     house5house11Axis.addClass("house-axis-5-11");
     return house5house11Axis;
@@ -256,46 +261,53 @@ export class Drawer {
     const house6Degree = (this.houses.hasOwnProperty('house6') && this.houses.house6.hasOwnProperty('degree')) ? this.houses.house6.degree : null;
     const house6Point = Calc.getPointOnCircle(zodiac.radius.outer, house6Degree, -2);
     const house12Point = Calc.getPointOnCircle(zodiac.radius.outer, Calc.getOppositeDegree(house6Degree), -2);
-    const house6house12Axis = this.s.line(house6Point.x, house6Point.y, house12Point.x, house12Point.y);
+    const house6house12Axis = this.snap.line(house6Point.x, house6Point.y, house12Point.x, house12Point.y);
     house6house12Axis.addClass("house-axis");
     house6house12Axis.addClass("house-axis-6-12");
     return house6house12Axis;
   }
 
   drawPlanet(planet, degree) {
-    const planetImageWidth = 4;
-    const planetImageHeight = 4;
-
     const linePoint1 = Calc.getPointOnCircle(zodiac.radius.inner, degree);
     const linePoint2 = Calc.getPointOnCircle(zodiac.radius.inner, degree, 1);
-    const planetAuxiliaryLine = this.s.line(linePoint1.x, linePoint1.y, linePoint2.x, linePoint2.y);
+    const planetAuxiliaryLine = this.snap.line(linePoint1.x, linePoint1.y, linePoint2.x, linePoint2.y);
     planetAuxiliaryLine.addClass("planet-auxiliary-line")
 
-    const planetPosition = Calc.getPointOnCircle(zodiac.radius.inner, degree, 3);
-    const planetImagePositionX = planetPosition.x - planetImageWidth / 2;
-    const planetImagePositionY = planetPosition.y - planetImageHeight / 2;
+    const planetBackgroundPosition = Calc.getPointOnCircle(zodiac.radius.inner, degree, this.PLANET_RADIUS_OFFSET);
+    const planetBackgroundRadius = this.getPlanetBackgroundRadius();
+    const planetBackground = this.snap.circle(planetBackgroundPosition.x, planetBackgroundPosition.y, planetBackgroundRadius);
+    planetBackground.addClass("planet-background");
 
-    let planetSymbolBackgroundRadius = null;
-    if (planetImageWidth > planetImageHeight) {
-      planetSymbolBackgroundRadius = planetImageWidth / 2;
-    } else {
-      planetSymbolBackgroundRadius = planetImageHeight / 2;
-    }
-    const planetSymbolBackground = this.s.circle(planetPosition.x, planetPosition.y, planetSymbolBackgroundRadius);
-    planetSymbolBackground.addClass("planet-background");
-
-    const planetSymbol = this.s.image(planet.imageUrl, planetImagePositionX, planetImagePositionY, planetImageWidth, planetImageHeight);
+    const planetSymbolPosition = this.getPlanetSymbolPosition(planetBackgroundPosition);
+    const planetSymbol = this.snap.image(planet.imageUrl, planetSymbolPosition.x, planetSymbolPosition.y, this.PLANET_IMAGE_WIDTH, this.PLANET_IMAGE_HEIGHT);
 
     const meta = {};
     Object.assign(meta, planet);
     meta['degree'] = degree;
-    meta['position'] = planetPosition;
+    meta['position'] = planetBackgroundPosition;
 
     return {
-      planet: planetSymbol,
-      background: planetSymbolBackground,
+      symbol: planetSymbol,
+      background: planetBackground,
       meta
     };
+  }
+
+  getPlanetSymbolPosition(planetBackgroundPosition) {
+    const x = planetBackgroundPosition.x - this.PLANET_IMAGE_WIDTH / 2;
+    const y = planetBackgroundPosition.y - this.PLANET_IMAGE_HEIGHT / 2;
+    return {
+      x,
+      y
+    }
+  }
+
+  getPlanetBackgroundRadius() {
+    if (this.PLANET_IMAGE_WIDTH > this.PLANET_IMAGE_HEIGHT) {
+      return this.PLANET_IMAGE_WIDTH / 2;
+    } else {
+      return this.PLANET_IMAGE_HEIGHT / 2;
+    }
   }
 
   drawSun() {
@@ -359,17 +371,60 @@ export class Drawer {
   }
 
   correctCollidingPlanets(planets) {
-    let planetsCoordinates = planets.map((planet) => {
-      return {
-        name: planet.name,
-        x: planet.drawing.position.x,
-        y: planet.drawing.position.y
-      }
-    }).sort((a, b) => {
-      return a.x > b.x && a.y > b.y;
+    planets = planets.sort((a, b) => {
+      return a.meta.degree > b.meta.degree;
     });
 
-    return;
+    let planetsCollideInRow = 0;
+    return planets.map((planet, i) => {
+      const nextPlanetIndex = i + 1;
+
+      if (nextPlanetIndex in planets) {
+        const nextPlanet = planets[nextPlanetIndex];
+        if (this.planetsDoCollide(planet.meta.degree, nextPlanet.meta.degree)) {
+          if (planetsCollideInRow == this.MAX_PLANET_COLLISION_CORRECTION) {
+            planetsCollideInRow = 0;
+          }
+          planetsCollideInRow++;
+
+          const correctedRadius = zodiac.radius.inner - (planetsCollideInRow * this.PLANET_COLLISION_CORRECTION_RADIUS);
+          if (correctedRadius <= 0) {
+            console.warn("Cannot draw colliding planets when the correction radius is below 0.");
+          }
+
+          const correctedBackgroundPosition = Calc.getPointOnCircle(correctedRadius, nextPlanet.meta.degree, this.PLANET_RADIUS_OFFSET);
+          const correctedBackgroundPositionForCircle = {
+            cx: correctedBackgroundPosition.x,
+            cy: correctedBackgroundPosition.y,
+          }
+          nextPlanet.background.attr(correctedBackgroundPositionForCircle);
+
+          const correctedPlanetSymbolPosition = this.getPlanetSymbolPosition(correctedBackgroundPosition);
+          nextPlanet.symbol.attr(correctedPlanetSymbolPosition);
+
+
+          planets[nextPlanetIndex].symbol = nextPlanet.symbol;
+          planets[nextPlanetIndex].background = nextPlanet.background;
+        } else {
+          planetsCollideInRow = 0;
+        }
+      }
+
+      return planet;
+    });
+  }
+
+  planetsDoCollide(currentPlanetDegree, nextPlanetDegree) {
+    let lowerBound = currentPlanetDegree - this.PLANET_COLLISION_MARGIN_IN_DEGREE;
+    let upperBound = currentPlanetDegree + this.PLANET_COLLISION_MARGIN_IN_DEGREE;
+
+    if (upperBound > 360) {
+      upperBound = upperBound - 360;
+    } else if (lowerBound < 0) {
+      lowerBound = lowerBound + 360;
+    }
+
+    return (lowerBound <= nextPlanetDegree && nextPlanetDegree <= upperBound);
   }
 }
 export let drawer = new Drawer();
